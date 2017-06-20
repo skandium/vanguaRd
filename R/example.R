44c418a9a104f5eec285c36105c6345641ed6ad0
taivop <taivo.pungas@gmail.com>
2017-06-20 17:27:31 +0300

    remove unnecessary stuff

--- R/example.R
+++ R/example.R
@@ -4,13 +4,11 @@
                      astring="12312")
 
   vanguard_init("http://localhost:5080", "Taivo R dev project 1", "test experiment 3",
-                parameters, "R/testing.R", tags=c("tag1", "tag2"))
+                parameters, "R/example.R", tags=c("tag1", "tag2"))
 
-  #sink(stdout(), type = "message")
-  print("taivo leeet kesk")
+  print("this is a test message")
 
   send_metric("test metric", 42.123123)
   send_metric("param1", vanguard_settings$args$param1)
-  send_metric("astring", vanguard_settings$args$astring)
-  cat("taivo leeet lõpp")
-  #print(toJSON(.get_options_dict()))
+  send_metric("astring", vanguard_settings$args$astring) # TODO for some reason only the last metric I send is shown in FGLab?
+  cat("a final message")
