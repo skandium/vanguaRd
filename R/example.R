06bc2f8d0fa571811be1b9481d58cb97f63ffc6c
taivop <taivo.pungas@gmail.com>
2017-06-20 17:46:23 +0300

    more informative example

--- R/example.R
+++ R/example.R
@@ -7,8 +7,11 @@
                 parameters, "R/example.R", tags=c("tag1", "tag2"))
 
   print("this is a test message")
+  print(paste0("param1", vanguard_settings$args$param1))
+  print(paste0("astring", vanguard_settings$args$astring))
+  send_file("DESCRIPTION")
 
-  send_metric("test metric", 42.123123)
-  send_metric("param1", vanguard_settings$args$param1)
-  send_metric("astring", vanguard_settings$args$astring) # TODO for some reason only the last metric I send is shown in FGLab?
+  send_value("test metric", 42.123123)
+  send_value("param1", vanguard_settings$args$param1)
+  send_value("astring", vanguard_settings$args$astring) # TODO for some reason only the last metric I send is shown in FGLab?
   cat("a final message")
